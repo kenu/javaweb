55a6523368d856632a524c7d6629a8448967c3dc
shpark <lovehyun@hotmail.com>
2017-09-19 22:16:19 +0900

    removed errors

---
+++ simpleboard/src/main/java/simpleboard/SimpleBoard.java
@@ -0,0 +1,33 @@
+package simpleboard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleBoard {
+    List<Article> store = new ArrayList<Article>();
+
+    public boolean add(Article article) {
+        store.add(article);
+        return true;
+    }
+
+    public Article get(long l) {
+        return new Article();
+    }
+
+    public long size() {
+        return store.size();
+    }
+
+    public boolean delete(long l) {
+        return true;
+    }
+
+    public void update(Article article2) {
+
+    }
+
+    public List<Article> getList() {
+        return store;
+    }
+}
