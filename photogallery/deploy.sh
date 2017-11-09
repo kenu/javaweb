e0217aa719ad218de313a3da64c814120436586a
EC2 Default User <ec2-user@ip-172-31-0-88.ap-northeast-2.compute.internal>
2017-11-09 13:08:47 +0000

    photogallery start

---
+++ photogallery/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo service photoapp stop
+sleep 5;
+echo `pwd`
+
+APP=photogallery.war
+TARGETDIR=/home/ec2-user/
+
+cp -f photogallery/target/$APP $TARGETDIR$APP
+chmod 755 $TARGETDIR$APP
+
+sudo rm -rf /etc/init.d/photoapp
+sudo ln -s $TARGETDIR$APP /etc/init.d/photoapp
+sudo service photoapp start
